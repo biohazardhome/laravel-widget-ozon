90a8d19fbddbb4a60c6d9ced8fee6e24d79139c8
biohazardhome <biohazardhome@gmail.com>
2024-03-22 16:07:58 +0300

    Update OzonApi

--- src/Http/OzonApi.php
+++ src/Http/OzonApi.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Biohazard\Http;
+namespace App\Http;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Response;
 use Carbon\Carbon;
+use stdClass;
 
 class OzonApi
 {
@@ -20,146 +21,49 @@ class OzonApi
     {
         $response = Http::ozonApi()->get($url, $query);
         $this->response = $response;
-        return $response->json();
+        
+        if ($response->successful()) {
+            return $response->json();
+        } else {
+            $response->throw();
+        }
     }
 
-    public function post(string $url, array $query = []): array
+    public function post(string $url, array|object $query = []): array
     {
         $response = Http::ozonApi()->post($url, $query);
         $this->response = $response;
-        return $response->json();
+
+        if ($response->successful()) {
+            $result = $response->json();
+            return $result;
+        } else {
+            $response->throw();
+        }
     }
 
     public function getResponse(): Response {
         return $this->response;
     }
 
-    public function productInfoStocksV3(array $filter, $last_id = '', int $limit = 1000): array {
-        return $this->post('/v3/product/info/stocks', [
+    public function productList(object $filter = new stdClass, int $limit = 1000, string $lastId = null): array {
+        return $this->post('/v2/product/list', [
             'filter' => $filter,
-            'last_id' => $last_id,
+            'last_id' => $lastId,
             'limit' => $limit,
-        ]);
+        ])['result'];
     }
 
-    public function postingFboListV2(array $filter, string $dir = 'asc', int $limit = 1000, int $offset = 0, bool $translit = false, array $with = []): array {
-        return $this->post('/v2/posting/fbo/list', [
-            'dir' => $dir,
-            'filter' => $filter,
-            'limit' => $limit,
-            'offset' => $offset,
-            'translit' => $translit,
-            // 'with' => $with,
-        ]);
+    public function productInfo(int $product_id, string $offer_id = null, int $sku = null): array {
+        return $this->post('/v2/product/info', [
+            'product_id' => $product_id,
+            'offer_id' => $offer_id,
+            'sku' => $sku,
+        ])['result'];
     }
 
-    public function postingFbsListV3(array $filter, int $offset = 0, string $dir = 'asc', int $limit = 1000, array $with = []): array {
-        return $this->post('v3/posting/fbs/list', [
-            'dir' => $dir,
-            'filter' => $filter,
-            'limit' => $limit,
-            'offset' => $offset,
-            'with' => [
-                'analytics_data' => true,
-                'barcodes' => true,
-                'financial_data' => true,
-            ],
-        ]);
+    public function ratingSummary(): array {
+        return $this->post('/v1/rating/summary', new \stdClass)['groups'];
     }
 
-    public function posts(Carbon $dateSince) { 
-        $dates = $this->dates($dateSince);
-        $postings = [];
-        foreach ($dates as [$dateSince, $dateTo]) {
-            dump($dateSince->toRfc3339String(), $dateTo->toRfc3339String());
-            $postings = array_merge($postings, $this->postsDate($dateSince, $dateTo));
-        }
-
-        return $postings;
-    }
-
-    public function dates(Carbon $dateSince) {
-        $dates = [];
-
-        $dateTo = Carbon::now();
-
-        $diff = $dateTo->floatDiffInYears($dateSince);
-        if ($diff > 1) {
-            for($i = 0; $i < floor($diff); $i++) {
-                $dateTo = clone($dateSince);
-                // if ($i === 0) {
-                    $dateTo->addYear(1)->subSecond(1);
-                // }
-                $dates[] = [$dateSince, $dateTo];
-                $dateSince = $dateTo;
-            }
-            $dateTo = Carbon::now();
-            // $dates[] = [$dateSince, $dateTo];
-        }/* else {
-            // $dates[] = [$dateSince, $dateTo];
-        }*/
-        
-        $dates[] = [$dateSince, $dateTo];
-
-        return $dates;
-    }
-
-    public function postsDate(Carbon $dateSince, Carbon $dateTo) {
-        $result = $this->postsRequest($dateSince, $dateTo);
-        // $response = $this->getResponse();
-        $i = 1;
-
-        while ($result['has_next']) {
-            $result2 = $this->postsRequest($dateSince, $dateTo, $i * 1000);
-            $postings = array_merge($result['postings'], $result2['postings']);
-            $result['postings'] = $postings;
-            $result['has_next'] = $result2['has_next'];
-
-            $i++;
-        }
-        
-        return $result['postings'];
-    }
-
-    public function postsRequest(Carbon $dateSince, Carbon $dateTo, int $offset = 0) {
-        $result = $this->postingFbsListV3([
-            'delivery_method_id' => [],
-            'provider_id' => [],
-            'since' => $dateSince->toRfc3339String(),
-            'status' => '',
-            'to' => $dateTo->toRfc3339String(),
-            'status' => '',
-            'warehouse_id' => [],
-        ],
-        $offset/*,
-        with: [
-            'analytics_data' => true,
-            'barcodes' => true,
-            'financial_data' => true,
-        ]*/);
-        $response = $this->getResponse();
-
-        if ($response->successful()) {
-            return $result['result'];
-        } else {
-            $response->throw();
-        }
-    }
-
-    /*public function postsParallel(Carbon $dateSince) { 
-        $dates = $this->dates($dateSince);
-        $postings = [];
-        foreach ($dates as [$dateSince, $dateTo]) {
-            $p = new \parallel\Runtime();
-            $future = $p->run(function() use ($dateSince, $dateTo) {
-                dump($dateSince->toRfc3339String(), $dateTo->toRfc3339String());
-                $postings = $this->postsDate($dateSince, $dateTo);
-                return $postings;
-            });
-
-            $postings = array_merge($postings, $future->value());
-        }
-
-        return $postings;
-    }*/
 }
