e9cc54b49195d51396d19e9c2ae229621ebd6111
biohazardhome <biohazardhome@gmail.com>
2024-03-24 13:44:45 +0300

    Add leaf slides on mousemove event

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
+    let swiper = new Swiper('.widget-product .swiper', {
         slidesPerView: 1,
         lazyPreloadPrevNext: 1,
         mousewheel: true,
         loop: true,
-        autoplay: {
+        /*autoplay: {
             delay: 5000,
-            pauseOnMouseEnter: true,
-        },
+        },*/
+        on: {
+            init: function() {
+                this.autoplay.stop();
+            },
+        }
+    });
+
+    /*let products = document.querySelectorAll('.widget-product');
+    products.forEach(function(el, index) {
+        el.addEventListener('mouseenter', function() {
+            swiper[index].autoplay.start();
+        });
+
+        el.addEventListener('mouseleave', function() {
+            swiper[index].autoplay.stop();
+        });
+    });*/
+
+    let products = document.querySelectorAll('.widget-product .swiper');
+    products.forEach(function(el, index) {
+        el.addEventListener('mousemove', function(e) {
+            let sw = swiper[index],
+                widthPerSlide = sw.width / sw.slides.length,
+                slideIndex = e.offsetX / widthPerSlide;
+
+            sw.slideTo(slideIndex, 0);
+
+            e.stopPropagation();
+        });
+
+        el.addEventListener('mouseleave', function() {
+            swiper[index].slideTo(0, 0);
+        });
     });
 });
